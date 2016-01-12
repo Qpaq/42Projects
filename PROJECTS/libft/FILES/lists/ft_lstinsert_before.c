0712b005e1013a1d09fc447462fce57323159e96
David Tedgui <teddav@users.noreply.github.com>
2016-01-12 19:03:04 +0100

    libft: modif ft_lstinsert

--- PROJECTS/libft/FILES/lists/ft_lstinsert_before.c
+++ PROJECTS/libft/FILES/lists/ft_lstinsert_before.c
@@ -2,4 +2,19 @@
 
 void	ft_lstinsert_before(t_list **alst, t_list *target, t_list *new)
 {
+  t_list  *previous;
+  t_list  *node;
+  
+  node = *alst;
+  previous = *alst;
+  while (node)
+  {
+    if (node == target)
+    {
+      new->next = target;
+      previous->next = new;
+    }
+    previous = node;
+    node = node->next;
+  }
 }
