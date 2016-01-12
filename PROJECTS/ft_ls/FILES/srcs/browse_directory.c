99168aac931fae0631044726619d914847e22bb3
David Tedgui <teddav@users.noreply.github.com>
2016-01-12 19:11:20 +0100

    ft_ls: browse_directory started to implement -R

--- PROJECTS/ft_ls/FILES/srcs/browse_directory.c
+++ PROJECTS/ft_ls/FILES/srcs/browse_directory.c
@@ -12,7 +12,7 @@
 
 #include "ft_ls.h"
 
-t_file_infos	*read_directory(char *dir_name, t_ls_args *args)
+t_file_infos	*read_directory(t_dir_infos current_dir, t_ls_args *args)
 {
 	DIR				*dirp;
 	struct dirent	*dir_entry;
@@ -21,18 +21,23 @@ t_file_infos	*read_directory(char *dir_name, t_ls_args *args)
 	t_file_infos	*head_list;
 
 	head_list = NULL;
-	if (!(dirp = opendir(dir_name)))
+	if (!(dirp = opendir(current_dir->name)))
 		return (NULL);
 	while ((dir_entry = readdir(dirp)))
 	{
 		if (!ft_strchr(args->options, 'a') && dir_entry->d_name[0] == '.')
 			continue ;
 		full_name = ft_strjoin("/", dir_entry->d_name);
-		full_name = ft_strjoin(dir_name, full_name);
+		full_name = ft_strjoin(current_dir->name, full_name);
 		if (!head_list)
 		{
 			head_list = get_file_info(full_name);
 			current_file = head_list;
+			
+			/*
+			if (head_list->type == 'd')
+				ft_lstinsert_after(args->dirs, current_dir)
+			*/
 			current_file->name = ft_strdup(dir_entry->d_name);
 		}
 		else
@@ -54,7 +59,10 @@ int				browse_directories(t_ls_args *args)
 	current_dir = args->dirs;
 	while (current_dir)
 	{
-		if ((files_list = read_directory(current_dir->name, args)))
+		/*
+		modifier le prototype de read_directory()
+		*/
+		if ((files_list = read_directory(current_dir, args)))
 		{
 			if (ft_strchr(args->options, 'l'))
 				print_dir_long(files_list, current_dir->name);
