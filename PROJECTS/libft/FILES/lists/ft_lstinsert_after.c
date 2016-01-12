fda24167e501ccf2d16f6710a0a6d6774771d142
David TEDGUI <dtedgui@e3r7p12.42.fr>
2016-01-12 09:28:09 +0100

    libft: adding lists functions

--- PROJECTS/libft/FILES/lists/ft_lstinsert_after.c
+++ PROJECTS/libft/FILES/lists/ft_lstinsert_after.c
@@ -2,4 +2,14 @@
 
 void	ft_lstinsert_after(t_list **alst, t_list *target, t_list *new)
 {
+	if (*alst)
+	{
+		if (!target)
+			ft_lstadd_end(alst, new);
+		else
+		{
+			new->next = target->next;
+			target->next = new;
+		}
+	}
 }
