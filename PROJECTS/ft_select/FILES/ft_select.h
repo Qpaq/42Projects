51d82ca57eccc077cc63673fd46cf35b392ec723
David <dtedgui@gmail.com>
2016-02-07 21:35:21 +0100

    ft_select

--- PROJECTS/ft_select/FILES/ft_select.h
+++ PROJECTS/ft_select/FILES/ft_select.h
@@ -16,5 +16,6 @@
 # include "libft/libft.h"
 # include <unistd.h>
 # include <stdlib.h>
+# include <signal.h>
 
 #endif
--- PROJECTS/ft_select/FILES/srcs/main.c
+++ PROJECTS/ft_select/FILES/srcs/main.c
@@ -1,6 +1,11 @@
 #include "ft_select.h"
 
+void	get_signal(void)
+{
+}
+
 int		main(void)
 {
+
 	return (0);
 }
