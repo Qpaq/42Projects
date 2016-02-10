2899790ca5e1ba8219893bd7c54a6a74d65d5319
David <dtedgui@gmail.com>
2016-02-10 10:27:59 +0100

    ft_select: sigwinch

--- PROJECTS/ft_select/FILES/srcs/signals.c
+++ PROJECTS/ft_select/FILES/srcs/signals.c
@@ -6,7 +6,7 @@
 /*   By: dtedgui <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/08 12:11:41 by dtedgui           #+#    #+#             */
-/*   Updated: 2016/02/09 11:16:43 by dtedgui          ###   ########.fr       */
+/*   Updated: 2016/02/10 09:45:55 by dtedgui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,26 @@ void	catch_signal(int code)
 	}
 }
 
+void	check_window_size(int code)
+{
+	int		width;
+	int		height;
+
+	if (code == SIGWINCH)
+	{
+		width = tgetnum("co");
+		height = tgetnum("li");
+		ft_printf("cols: %d, lines: %d\n", width, height);
+	}
+}
+
 void	ft_signals(void)
 {
 	signal(SIGINT, catch_signal);
 	signal(SIGQUIT, catch_signal);
 	signal(SIGKILL, catch_signal);
 	signal(SIGCONT, catch_signal);
+	signal(SIGWINCH, check_window_size);
 //	signal(SIGTSTP, catch_signal);
 //	signal(SIGSEGV, catch_signal);
 }
