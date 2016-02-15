1e0b81f0564aeaa1a955b55c64ddcd51c4399529
David TEDGUI <dtedgui@gmail.com>
2016-02-15 11:16:03 +0100

    test: testing MAX values

--- tests/types_limits.c
+++ tests/types_limits.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <limits.h>
 
 int		main(void)
 {
@@ -15,4 +16,11 @@ int		main(void)
 	printf("float %lu\n", sizeof(float));
 	printf("uintmax %lu\n", sizeof(uintmax_t));
 	printf("intmax %lu\n", sizeof(intmax_t));
+
+// ============================================================
+
+	printf("\nMAXIMUMS\n");
+	printf("uintmax: %u, intmax: %i\n", UINT_MAX, INT_MAX);
+	printf("longmax: %li, llongmax: %lli\n", LONG_MAX, LLONG_MAX);
+	printf("ulongmax: %lu, ullongmax: %llu\n", ULONG_MAX, ULLONG_MAX);
 }
