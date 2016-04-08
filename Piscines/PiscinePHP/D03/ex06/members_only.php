5827f039bc149be99396ab300a3206f5ce742a6e
David TEDGUI <dtedgui@gmail.com>
2016-04-08 11:42:48 +0200

    php: day4

--- Piscines/PiscinePHP/D03/ex06/members_only.php
+++ Piscines/PiscinePHP/D03/ex06/members_only.php
@@ -1,25 +1,30 @@
 <?php
 session_start();
 
-$user = '';
-$pass = '';
-header('WWW-Authenticate: Basic '. base64_encode($user.":".$pass));
-//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-if (!isset($_SESSION['']))
-{
+$user = 'zaz';
+$pass = 'jaimelespetitsponeys';
+header('Content-Type: text/html');
 
+//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+if (!isset($_SERVER['PHP_AUTH_USER']))
+{
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo '<html><body>Cette zone est accessible uniquement aux membres du site</body></html>';
+    exit();
+}
+else
+{
+	if (USEROK)
+	{
+		header('WWW-Authenticate: Basic '. base64_encode($user.":".$pass));
+	    echo "<p>Bonjour, {$_SERVER['PHP_AUTH_USER']}.</p>";
+	    echo "<p>Votre mot de passe est {$_SERVER['PHP_AUTH_PW']}.</p>";
+	}
+	else
+	{
+	}
 }
 
 ?>
-/*
-if (!isset($_SERVER['PHP_AUTH_USER'])) {
-    header('WWW-Authenticate: Basic realm="My Realm"');
-    header('HTTP/1.0 401 Unauthorized');
-    echo 'Texte utilisé si le visiteur utilise le bouton d\'annulation';
-    exit;
-} else {
-    echo "<p>Bonjour, {$_SERVER['PHP_AUTH_USER']}.</p>";
-    echo "<p>Votre mot de passe est {$_SERVER['PHP_AUTH_PW']}.</p>";
-}
-
-*/
