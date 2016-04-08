19f8f9f52f83f351f59348527cc7dba5520d76c6
David TEDGUI <dtedgui@gmail.com>
2016-04-08 08:31:04 +0200

    php

--- Piscines/PiscinePHP/D03/ex06/members_only.php
+++ Piscines/PiscinePHP/D03/ex06/members_only.php
@@ -1,12 +1,25 @@
 <?php
 session_start();
 
+$user = '';
+$pass = '';
+header('WWW-Authenticate: Basic '. base64_encode($user.":".$pass));
+//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+if (!isset($_SESSION['']))
+{
+
+}
 
 ?>
 /*
- * $headers = array(
- *     'Content-Type:application/json',
- *     'Authorization: Basic '. base64_encode("user:password")
- *     );
- * curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Texte utilisé si le visiteur utilise le bouton d\'annulation';
+    exit;
+} else {
+    echo "<p>Bonjour, {$_SERVER['PHP_AUTH_USER']}.</p>";
+    echo "<p>Votre mot de passe est {$_SERVER['PHP_AUTH_PW']}.</p>";
+}
+
 */
