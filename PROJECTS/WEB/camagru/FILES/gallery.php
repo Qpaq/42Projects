eeddf8e49869ce1df5ee661d02b00bbac1d4a442
David TEDGUI <dtedgui@gmail.com>
2016-04-16 23:01:33 +0200

    Camagru

--- PROJECTS/WEB/camagru/FILES/gallery.php
+++ PROJECTS/WEB/camagru/FILES/gallery.php
@@ -13,11 +13,25 @@ if (empty($photos)) {
 					<img src="<?php echo $photo['path']; ?>" />
 				</td>
 				<td>
-					<button class="like" value="<?php echo $photo['id']; ?>">LIKE</button>
-					<textarea class="comment_text" name="<?php echo $photo['id']; ?>" placeholder="Enter comment here"></textarea><br />
-					<button class="comment_button" value="<?php echo $photo['id']; ?>">COMMENT</button>
+					<p>
+						<button class="like" onclick="likePhoto(this);" value="<?php echo $photo['id']; ?>">&#9829;</button>
+					</p>
+					<p>
+						<textarea class="comment_text" name="<?php echo $photo['id']; ?>" placeholder="Enter comment here"></textarea><br />
+						<button class="comment_button" onclick="commentPhoto(this);" value="<?php echo $photo['id']; ?>">COMMENT</button>
+					</p>
 				</td>
 			</tr>
 		<?php } ?>
 	</table>
 </section>
+
+<script type="text/javascript">
+function likePhoto(obj) {
+	console.log(obj.value);
+}
+function commentPhoto(obj) {
+	var comment = document.querySelector("textarea[name='"+ obj.value +"']");
+	console.log(comment.value);
+}
+</script>
